b2ea8d3a4b047716ab357b4cac9b020975bea323
Gustaaf Vandeboel <gustaaf.vandeboel@gim.be>
2018-06-22 16:17:14 +0200

    Adding copy of existing harvesting and rename it to dcatap harvester

---
+++ src/main/java/org/fao/geonet/kernel/harvest/harvester/dcatap/Aligner.java
@@ -0,0 +1,357 @@
+//=============================================================================
+//===	Copyright (C) 2001-2013 Food and Agriculture Organization of the
+//===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
+//===	and United Nations Environment Programme (UNEP)
+//===
+//===	This program is free software; you can redistribute it and/or modify
+//===	it under the terms of the GNU General Public License as published by
+//===	the Free Software Foundation; either version 2 of the License, or (at
+//===	your option) any later version.
+//===
+//===	This program is distributed in the hope that it will be useful, but
+//===	WITHOUT ANY WARRANTY; without even the implied warranty of
+//===	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//===	General Public License for more details.
+//===
+//===	You should have received a copy of the GNU General Public License
+//===	along with this program; if not, write to the Free Software
+//===	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//===
+//===	Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+//===	Rome - Italy. email: geonetwork@osgeo.org
+//==============================================================================
+
+package org.fao.geonet.kernel.harvest.harvester.dcatap;
+
+import jeeves.server.context.ServiceContext;
+
+import org.fao.geonet.GeonetContext;
+import org.fao.geonet.Logger;
+import org.fao.geonet.constants.Geonet;
+import org.fao.geonet.domain.ISODate;
+import org.fao.geonet.domain.Metadata;
+import org.fao.geonet.domain.MetadataType;
+import org.fao.geonet.domain.OperationAllowedId_;
+import org.fao.geonet.kernel.DataManager;
+import org.fao.geonet.kernel.UpdateDatestamp;
+import org.fao.geonet.kernel.harvest.BaseAligner;
+import org.fao.geonet.kernel.harvest.harvester.CategoryMapper;
+import org.fao.geonet.kernel.harvest.harvester.GroupMapper;
+import org.fao.geonet.kernel.harvest.harvester.HarvestError;
+import org.fao.geonet.kernel.harvest.harvester.HarvestResult;
+import org.fao.geonet.kernel.harvest.harvester.RecordInfo;
+import org.fao.geonet.kernel.harvest.harvester.UUIDMapper;
+import org.fao.geonet.kernel.harvest.harvester.dcatap.DCATAPParams;
+import org.fao.geonet.repository.MetadataRepository;
+import org.fao.geonet.repository.OperationAllowedRepository;
+import org.fao.geonet.utils.GeonetHttpRequestFactory;
+import org.fao.geonet.utils.Xml;
+import org.fao.geonet.utils.XmlRequest;
+import org.jdom.Element;
+
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+//=============================================================================
+
+public class Aligner extends BaseAligner {
+    //--------------------------------------------------------------------------
+    //---
+    //--- Constructor
+    //---
+    //--------------------------------------------------------------------------
+
+    private Logger log;
+
+    //--------------------------------------------------------------------------
+    //---
+    //--- Alignment method
+    //---
+    //--------------------------------------------------------------------------
+    private ServiceContext context;
+
+    //--------------------------------------------------------------------------
+    //---
+    //--- Private methods : addMetadata
+    //---
+    //--------------------------------------------------------------------------
+    private XmlRequest request;
+
+    //--------------------------------------------------------------------------
+    //---
+    //--- Private methods : updateMetadata
+    //---
+    //--------------------------------------------------------------------------
+    private DCATAPParams params;
+
+    //--------------------------------------------------------------------------
+    //---
+    //--- Private methods
+    //---
+    //--------------------------------------------------------------------------
+    private DataManager dataMan;
+
+    //--------------------------------------------------------------------------
+    private CategoryMapper localCateg;
+
+    //--------------------------------------------------------------------------
+    //---
+    //--- Variables
+    //---
+    //--------------------------------------------------------------------------
+    private GroupMapper localGroups;
+    private UUIDMapper localUuids;
+    private HarvestResult result;
+    public Aligner(AtomicBoolean cancelMonitor, Logger log, ServiceContext sc, DCATAPParams params) throws Exception {
+        super(cancelMonitor);
+        this.log = log;
+        this.context = sc;
+        this.params = params;
+
+        GeonetContext gc = (GeonetContext) context.getHandlerContext(Geonet.CONTEXT_NAME);
+        dataMan = gc.getBean(DataManager.class);
+        result = new HarvestResult();
+
+        //--- setup REST operation rest/document?id={uuid}
+
+        request = context.getBean(GeonetHttpRequestFactory.class).createXmlRequest(new URL(params.baseUrl + "/rest/document"));
+
+    }
+
+    public HarvestResult align(Set<RecordInfo> records, List<HarvestError> errors) throws Exception {
+        log.info("Start of alignment for : " + params.getName());
+
+        //-----------------------------------------------------------------------
+        //--- retrieve all local categories and groups
+        //--- retrieve harvested uuids for given harvesting node
+
+        localCateg = new CategoryMapper(context);
+        localGroups = new GroupMapper(context);
+        localUuids = new UUIDMapper(context.getBean(MetadataRepository.class), params.getUuid());
+
+        dataMan.flush();
+
+        //-----------------------------------------------------------------------
+        //--- remove old metadata
+
+        for (String uuid : localUuids.getUUIDs()) {
+            if (cancelMonitor.get()) {
+                return result;
+            }
+
+            if (!exists(records, uuid)) {
+                String id = localUuids.getID(uuid);
+
+                if (log.isDebugEnabled())
+                    log.debug("  - Removing old metadata with local id:" + id);
+                dataMan.deleteMetadata(context, id);
+
+                dataMan.flush();
+
+                result.locallyRemoved++;
+            }
+        }
+
+        //-----------------------------------------------------------------------
+        //--- insert/update new metadata
+
+        for (RecordInfo ri : records) {
+            if (cancelMonitor.get()) {
+                return result;
+            }
+
+            try {
+                String id = dataMan.getMetadataId(ri.uuid);
+
+                if (id == null) addMetadata(ri);
+                else updateMetadata(ri, id);
+                result.totalMetadata++;
+
+            }catch (Throwable t) {
+                errors.add(new HarvestError(context, t, log));
+                log.error("Unable to process record from csw (" + this.params.getName() + ")");
+                log.error("   Record failed: " + ri.uuid);
+            }
+        }
+
+        dataMan.forceIndexChanges();
+
+        log.info("End of alignment for : " + params.getName());
+
+        return result;
+    }
+
+    private void addMetadata(RecordInfo ri) throws Exception {
+        Element md = retrieveMetadata(ri.uuid);
+
+        if (md == null) return;
+
+        String schema = dataMan.autodetectSchema(md, null);
+
+        if (schema == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("  - Metadata skipped due to unknown schema. uuid:" + ri.uuid);
+            }
+            result.unknownSchema++;
+            return;
+        }
+
+        if (log.isDebugEnabled())
+            log.debug("  - Adding metadata with remote uuid:" + ri.uuid + " schema:" + schema);
+
+        //
+        // insert metadata
+        //
+        int userid = 1;
+        Metadata metadata = new Metadata().setUuid(ri.uuid);
+        metadata.getDataInfo().
+            setSchemaId(schema).
+            setRoot(md.getQualifiedName()).
+            setType(MetadataType.METADATA).
+            setChangeDate(new ISODate(ri.changeDate)).
+            setCreateDate(new ISODate(ri.changeDate));
+        metadata.getSourceInfo().
+            setSourceId(params.getUuid()).
+            setOwner(userid);
+        metadata.getHarvestInfo().
+            setHarvested(true).
+            setUuid(params.getUuid());
+
+        addCategories(metadata, params.getCategories(), localCateg, context, log, null, false);
+
+        metadata = dataMan.insertMetadata(context, metadata, md, true, false, false, UpdateDatestamp.NO, false, false);
+
+        String id = String.valueOf(metadata.getId());
+
+        addPrivileges(id, params.getPrivileges(), localGroups, dataMan, context, log);
+
+        dataMan.indexMetadata(id, Math.random() < 0.01, null);
+        result.addedMetadata++;
+    }
+
+    private void updateMetadata(RecordInfo ri, String id) throws Exception {
+        String date = localUuids.getChangeDate(ri.uuid);
+
+        if (date == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("  - Skipped metadata managed by another harvesting node. uuid:" + ri.uuid + ", name:" + params.getName());
+            }
+        } else {
+            if (log.isDebugEnabled()) {
+                log.debug("  - Comparing date " + date + " with harvested date " + ri.changeDate + " Comparison: " + ri.isMoreRecentThan(date));
+            }
+            if (!ri.isMoreRecentThan(date)) {
+                if (log.isDebugEnabled()) {
+                    log.debug("  - Metadata XML not changed for uuid:" + ri.uuid);
+                }
+                result.unchangedMetadata++;
+            } else {
+                if (log.isDebugEnabled()) {
+                    log.debug("  - Updating local metadata for uuid:" + ri.uuid);
+                }
+                Element md = retrieveMetadata(ri.uuid);
+
+                if (md == null) return;
+
+                //
+                // update metadata
+                //
+                boolean validate = false;
+                boolean ufo = false;
+                boolean index = false;
+                String language = context.getLanguage();
+                final Metadata metadata = dataMan.updateMetadata(context, id, md, validate, ufo, index, language, ri.changeDate, false);
+
+                OperationAllowedRepository repository = context.getBean(OperationAllowedRepository.class);
+                repository.deleteAllByIdAttribute(OperationAllowedId_.metadataId, Integer.parseInt(id));
+                addPrivileges(id, params.getPrivileges(), localGroups, dataMan, context, log);
+
+                metadata.getMetadataCategories().clear();
+                addCategories(metadata, params.getCategories(), localCateg, context, log, null, true);
+                dataMan.flush();
+
+                dataMan.indexMetadata(id, Math.random() < 0.01, null);
+                result.updatedMetadata++;
+            }
+        }
+    }
+
+    /**
+     * Returns true if the uuid is present in the remote node.
+     */
+    private boolean exists(Set<RecordInfo> records, String uuid) {
+        for (RecordInfo ri : records) {
+            if (uuid.equals(ri.uuid)) return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Does REST document request. If validation is requested and the metadata does not validate,
+     * null is returned. If transformation is requested then metadata is transformed.
+     *
+     * @param uuid uuid of metadata to request
+     * @return metadata the metadata
+     */
+    private Element retrieveMetadata(String uuid) {
+        request.clearParams();
+        //request.addParam("id","{"+uuid+"}");
+        request.addParam("id", uuid);
+
+        try {
+            if (log.isDebugEnabled()) {
+                log.debug("Getting record from : " + request.getHost() + " (uuid:" + uuid + ")");
+            }
+            Element response = null;
+            try {
+                response = request.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+                log.error("Getting record from GeoPortal REST raised exception: " + e.getMessage());
+                log.error("Sent request " + request.getSentData());
+                throw new Exception(e);
+            }
+
+            if (log.isDebugEnabled()) {
+                log.debug("Record got:\n" + Xml.getString(response));
+            }
+
+            try {
+                params.getValidate().validate(dataMan, context, response);
+            } catch (Exception e) {
+                log.info("Ignoring invalid metadata with uuid " + uuid);
+                result.doesNotValidate++;
+                return null;
+            }
+
+            // transform it here if requested
+            if (!params.getImportXslt().equals("none")) {
+                Path thisXslt = context.getAppPath().resolve(Geonet.Path.IMPORT_STYLESHEETS).
+                    resolve(params.getImportXslt());
+                try {
+                    response = Xml.transform(response, thisXslt);
+                } catch (Exception e) {
+                    log.info("Cannot transform XML " + Xml.getString(response) + ", ignoring. Error was: " + e.getMessage());
+                    result.badFormat++;
+                    return null;
+                }
+            }
+            return response;
+        } catch (Exception e) {
+            log.warning("Raised exception while getting record : " + e);
+            e.printStackTrace();
+            result.unretrievable++;
+
+            //--- we don't raise any exception here. Just try to go on
+            return null;
+        }
+    }
+}
+
+//=============================================================================
+
+
---
+++ src/main/java/org/fao/geonet/kernel/harvest/harvester/dcatap/DCATAPHarvester.java
@@ -0,0 +1,154 @@
+//=============================================================================
+//===	Copyright (C) 2001-2013 Food and Agriculture Organization of the
+//===	United Nations (FAO-UN), United Nations World Food Programme (WFP)
+//===	and United Nations Environment Programme (UNEP)
+//===
+//===	This program is free software; you can redistribute it and/or modify
+//===	it under the terms of the GNU General Public License as published by
+//===	the Free Software Foundation; either version 2 of the License, or (at
+//===	your option) any later version.
+//===
+//===	This program is distributed in the hope that it will be useful, but
+//===	WITHOUT ANY WARRANTY; without even the implied warranty of
+//===	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//===	General Public License for more details.
+//===
+//===	You should have received a copy of the GNU General Public License
+//===	along with this program; if not, write to the Free Software
+//===	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+//===
+//===	Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+//===	Rome - Italy. email: geonetwork@osgeo.org
+//==============================================================================
+
+package org.fao.geonet.kernel.harvest.harvester.dcatap;
+
+import jeeves.server.context.ServiceContext;
+
+import org.fao.geonet.Logger;
+import org.fao.geonet.domain.Source;
+import org.fao.geonet.exceptions.BadInputEx;
+import org.fao.geonet.kernel.harvest.harvester.AbstractHarvester;
+import org.fao.geonet.kernel.harvest.harvester.AbstractParams;
+import org.fao.geonet.kernel.harvest.harvester.HarvestResult;
+import org.fao.geonet.kernel.harvest.harvester.dcatap.DCATAPParams;
+import org.fao.geonet.kernel.harvest.harvester.dcatap.Harvester;
+import org.fao.geonet.kernel.harvest.harvester.dcatap.Search;
+import org.fao.geonet.repository.SourceRepository;
+import org.fao.geonet.resources.Resources;
+import org.jdom.Element;
+
+import java.io.File;
+import java.sql.SQLException;
+import java.util.UUID;
+
+//=============================================================================
+
+public class DCATAPHarvester extends AbstractHarvester<HarvestResult> {
+
+    //--------------------------------------------------------------------------
+    //---
+    //--- Init
+    //---
+    //--------------------------------------------------------------------------
+
+    private DCATAPParams params;
+
+    //---------------------------------------------------------------------------
+    //---
+    //--- Add
+    //---
+    //---------------------------------------------------------------------------
+
+    protected void doInit(Element node, ServiceContext context) throws BadInputEx {
+        params = new DCATAPParams(dataMan);
+        super.setParams(params);
+        params.create(node);
+    }
+
+    //---------------------------------------------------------------------------
+    //---
+    //--- Update
+    //---
+    //---------------------------------------------------------------------------
+
+    protected String doAdd(Element node) throws BadInputEx, SQLException {
+        params = new DCATAPParams(dataMan);
+        super.setParams(params);
+
+        //--- retrieve/initialize information
+        params.create(node);
+
+        //--- force the creation of a new uuid
+        params.setUuid(UUID.randomUUID().toString());
+
+        String id = harvesterSettingsManager.add("harvesting", "node", getType());
+
+        storeNode(params, "id:" + id);
+        Source source = new Source(params.getUuid(), params.getName(), params.getTranslations(), true);
+        context.getBean(SourceRepository.class).save(source);
+        Resources.copyLogo(context, "images" + File.separator + "harvesting" + File.separator + params.icon, params.getUuid());
+
+        return id;
+    }
+
+    //---------------------------------------------------------------------------
+
+    protected void doUpdate(String id, Element node) throws BadInputEx, SQLException {
+        DCATAPParams copy = params.copy();
+
+        //--- update variables
+        copy.update(node);
+
+        String path = "harvesting/id:" + id;
+
+        harvesterSettingsManager.removeChildren(path);
+
+        //--- update database
+        storeNode(copy, path);
+
+        //--- we update a copy first because if there is an exception DCATAPParams
+        //--- could be half updated and so it could be in an inconsistent state
+
+        Source source = new Source(copy.getUuid(), copy.getName(), copy.getTranslations(), true);
+        context.getBean(SourceRepository.class).save(source);
+        Resources.copyLogo(context, "images" + File.separator + "harvesting" + File.separator + copy.icon, copy.getUuid());
+
+        params = copy;
+        super.setParams(params);
+
+    }
+
+    //---------------------------------------------------------------------------
+    //---
+    //--- Harvest
+    //---
+    //---------------------------------------------------------------------------
+
+    protected void storeNodeExtra(AbstractParams p, String path,
+                                  String siteId, String optionsId) throws SQLException {
+        DCATAPParams params = (DCATAPParams) p;
+
+        harvesterSettingsManager.add("id:" + siteId, "baseUrl", params.baseUrl);
+        harvesterSettingsManager.add("id:" + siteId, "icon", params.icon);
+
+        //--- store search nodes
+
+        for (Search s : params.getSearches()) {
+            String searchID = harvesterSettingsManager.add(path, "search", "");
+
+            harvesterSettingsManager.add("id:" + searchID, "freeText", s.freeText);
+        }
+    }
+
+    //---------------------------------------------------------------------------
+    //---
+    //--- Variables
+    //---
+    //---------------------------------------------------------------------------
+
+    public void doHarvest(Logger log) throws Exception {
+        Harvester h = new Harvester(cancelMonitor, log, context, params);
+        result = h.harvest(log);
+    }
+}
